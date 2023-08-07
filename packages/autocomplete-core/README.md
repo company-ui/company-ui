2ed05d0c4627eaf67de247c00c13f2d88ebc81b5
MichaelSun <zhensherlock@126.com>
2023-08-07 14:18:43 +0800

    ci: add @company-ui/autocomplete rollup config

---
+++ packages/autocomplete-core/README.md
@@ -0,0 +1,3 @@
+# `@company-ui/autocomplete-core`
+
+暴露给 `@company-ui/autocomplete-dom`、 `@company-ui/autocomplete-vue`、 `@company-ui/autocomplete-react` 的 API。
---
+++ packages/autocomplete-core/rollup.config.js
@@ -0,0 +1,81 @@
+import filesize from 'rollup-plugin-filesize'
+import babel from '@rollup/plugin-babel'
+import resolve from '@rollup/plugin-node-resolve'
+import eslint from '@rollup/plugin-eslint'
+import terser from '@rollup/plugin-terser'
+import typescript from '@rollup/plugin-typescript'
+import strip from '@rollup/plugin-strip'
+
+const name = 'CompanyUIAutocompleteCore'
+
+const output = [
+  {
+    name,
+    format: 'esm',
+    file: 'dist/index.esm.js',
+    sourcemap: true
+  },
+  {
+    name,
+    format: 'umd',
+    file: 'dist/index.umd.js',
+    sourcemap: true
+  },
+  {
+    name,
+    format: 'iife',
+    file: 'dist/index.iife.js',
+    sourcemap: true
+  },
+  {
+    name,
+    format: 'cjs',
+    file: 'dist/index.cjs.js',
+    sourcemap: true
+  },
+  // min
+  {
+    name,
+    format: 'esm',
+    file: 'dist/index.esm.min.js',
+    plugins: [terser()]
+  },
+  {
+    name,
+    format: 'umd',
+    file: 'dist/index.umd.min.js',
+    plugins: [terser()]
+  },
+  {
+    name,
+    format: 'iife',
+    file: 'dist/index.iife.min.js',
+    plugins: [terser()]
+  },
+  {
+    name,
+    format: 'cjs',
+    file: 'dist/index.cjs.min.js',
+    plugins: [terser()]
+  }
+]
+
+export default [
+  {
+    input: 'src/main.ts',
+    output,
+    plugins: [
+      eslint({
+        throwOnError: true,
+        throwOnWarning: true,
+        include: ['src/**'],
+        exclude: ['node_modules/**', 'src/style/**']
+      }),
+      resolve(),
+      strip(),
+      typescript(),
+      babel({ babelHelpers: 'runtime', exclude: /node_modules/ }),
+      filesize()
+    ]
+  }
+]
