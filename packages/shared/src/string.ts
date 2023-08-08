a1a07fe499f8e45902d3b1e4f52b8a91188ec1d0
MichaelSun <zhensherlock@126.com>
2023-08-08 11:36:13 +0800

    feat: add @company-ui/shared string

---
+++ packages/shared/src/string.ts
@@ -0,0 +1,12 @@
+export const stringToJson = (str: string): any[] => {
+  try {
+    const result = JSON.parse(str)
+    if (Array.isArray(result)) {
+      return result
+    } else {
+      return []
+    }
+  } catch (e) {
+    return []
+  }
+}
