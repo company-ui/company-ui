c1a4840f8d5f440d41a2b6ac80b295e01b9ace32
MichaelSun <zhensherlock@126.com>
2023-08-08 11:40:28 +0800

    feat: add @company-ui/shared main

---
+++ packages/shared/src/main.ts
@@ -0,0 +1,7 @@
+export * from './click-outside'
+export * from './isFunction'
+export * from './isString'
+export * from './replaceAll'
+export * from './throttle'
+export * from './dom'
+export * from './string'
