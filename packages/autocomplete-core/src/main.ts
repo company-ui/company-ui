f694be01fd7cd2c3b50aeebe8c2c0b6000a5cfc6
MichaelSun <zhensherlock@126.com>
2023-08-09 16:15:15 +0800

    feat: add @company-ui/autocomplete-core main

---
+++ packages/autocomplete-core/src/main.ts
@@ -0,0 +1,10 @@
+import { addHistory, getHistory, removeHistory } from './history'
+export * from './avatar'
+export * from './api'
+export {
+  addHistory,
+  getHistory,
+  removeHistory
+}
+export * from './initialization'
+export type * from './types'
