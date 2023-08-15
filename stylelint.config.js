dc546123cc89de0f3c847cca695fc81e36183d1d
MichaelSun <zhensherlock@126.com>
2023-08-15 17:12:56 +0800

    feat: update stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   root: true,
   plugins: ['stylelint-order'],
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-standard-scss',
+    'stylelint-config-prettier',
+  ],
   rules: {
     'selector-pseudo-class-no-unknown': [
       true,
@@ -34,11 +38,11 @@ module.exports = {
     ],
     'no-empty-source': null,
     'named-grid-areas-no-invalid': null,
-    'unicode-bom': 'never',
+    // 'unicode-bom': 'never',
     'no-descending-specificity': null,
     'font-family-no-missing-generic-family-keyword': null,
-    'declaration-colon-space-after': 'always-single-line',
-    'declaration-colon-space-before': 'never',
+    // 'declaration-colon-space-after': 'always-single-line',
+    // 'declaration-colon-space-before': 'never',
     // 'declaration-block-trailing-semicolon': 'always',
     'rule-empty-line-before': [
       'always',
@@ -66,5 +70,5 @@ module.exports = {
       { severity: 'warning' },
     ],
   },
-  ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.tsx', '**/*.ts'],
+  ignoreFiles: ['**/*.js', '**/*.cjs', '**/*.jsx', '**/*.tsx', '**/*.ts'],
 };
